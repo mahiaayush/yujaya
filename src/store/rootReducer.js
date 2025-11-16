d6c89ea1c4a10a1822137c8f7128289d6e987e09
Alok Shakya <alok.shakya@3i-infotech.com>
2025-11-16 13:46:20 +0530

    import home reducer successfully!

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -3,6 +3,7 @@ import { getHomeReducer } from "./reducer/HomeReducer";
 import { getFaqReducer } from "./reducer/FaqReducer";
 import { getPrivacyContent } from "./action/PrivacyAction";
 import { getPrivacyReducer } from "./reducer/PrivacyReducer";
+import { getAboutUsReducer } from "./reducer/AboutUs";
 
 const rootReducer = combineReducers({
   getHomeContent: getHomeReducer,
